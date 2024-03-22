3fdb1883f072f7783419fd8df50ec3f8e4f05650
Ido Berkovich <idoberko2@gmail.com>
2024-03-22 11:24:48 +0200

    changed db config to connection string (#2)
    * changed db config to connection string
    
    * changed db config to connection string
    
    * changed go test action

--- db/connect_db.go
+++ db/connect_db.go
@@ -1,14 +1,12 @@
 package db
 
 import (
-	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 )
 
 func ConnectToDb(config DbConfig) (*sqlx.DB, error) {
-	db, err := sqlx.Connect("pgx", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		config.DbUser, config.DbPassword, config.DbHost, config.DbPort, config.DbName))
+	db, err := sqlx.Connect("pgx", config.DbConString)
 	if err != nil {
 		return nil, err
 	}
