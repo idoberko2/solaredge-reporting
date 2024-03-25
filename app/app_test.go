4b86197d22a7d633446a054cc6024fc38607372f
Ido Berkovich <idoberko2@gmail.com>
2024-03-25 16:38:19 +0200

    failing integration test

--- app/app_test.go
+++ app/app_test.go
@@ -1,44 +1,38 @@
-//go:build integration
-// +build integration
-
 package app
 
 import (
 	"context"
 	"fmt"
 	"github.com/idoberko2/semonitor/general"
+	"github.com/idoberko2/semonitor/server"
+	"github.com/imroc/req/v3"
+	log "github.com/sirupsen/logrus"
 	"net"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/suite"
 )
 
-/**
-This is a full E2E integration tests. It sets up the environment, sends a ping which updates the
-state to "Healthy", then waits 6 seconds for the state to become "Unhealthy".
-
-You should monitor the configured TG group when running the test. You expect to see:
-1. A "Healthy" message arrives immediately
-2. An "Unhealthy" message arrives 5 seconds later
-*/
-
 type IntegrationTestSuite struct {
 	suite.Suite
 	appCancel context.CancelFunc
 	srvPort   int
-	c         http.Client
+	c         *req.Client
+}
+
+func TestIntegrationSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
 }
 
 func (suite *IntegrationTestSuite) SetupSuite() {
+	general.InitBasePath()
 	suite.Require().NoError(general.LoadDotEnv())
 
-	suite.c = http.Client{}
-	suite.c.Timeout = 200 * time.Millisecond
+	suite.c = req.C().SetTimeout(200 * time.Millisecond)
 	suite.setupEnv()
 }
 
@@ -55,42 +49,23 @@ func (suite *IntegrationTestSuite) TearDownTest() {
 }
 
 func (suite *IntegrationTestSuite) waitUntilServerReady() {
-	var lastErr error
-	var lastStatusCode int
 	ticker := time.NewTicker(100 * time.Millisecond)
+	timeout := time.After(3 * time.Second)
 	for {
 		select {
 		case <-ticker.C:
-			{
-				resp, err := suite.c.Get(fmt.Sprintf("http://localhost:%d/is_alive", suite.srvPort))
-				lastErr = err
-				if resp != nil {
-					lastStatusCode = resp.StatusCode
-				}
-
-				if lastErr == nil && lastStatusCode == http.StatusOK {
-					return
-				}
+			if suite.isServerHealthy() {
+				log.Info("server is ready")
+				return
 			}
-		case <-time.After(3 * time.Second):
-			{
-				var errStr string
-				if lastErr != nil {
-					errStr = lastErr.Error()
-				}
-				suite.Assert().FailNowf("waited too long for server to be ready err=%s status_code=%d", errStr, lastStatusCode)
-			}
+		case <-timeout:
+			suite.Assert().FailNow("waited too long for server to be ready")
 		}
 	}
 }
 
 func (suite *IntegrationTestSuite) TestIntegration() {
-	passphrase := os.Getenv("HC_PASSPHRASE")
-	resp, err := suite.c.Post(fmt.Sprintf("http://localhost:%d/ping", suite.srvPort), "application/json", strings.NewReader(fmt.Sprintf("{\"passphrase\": \"%s\"}", passphrase)))
-	suite.Require().NoError(err)
-	suite.Assert().Equal(http.StatusOK, resp.StatusCode)
-
-	<-time.After(6 * time.Second)
+	suite.Assert().True(suite.isServerHealthy())
 }
 
 func (suite *IntegrationTestSuite) setupEnv() {
@@ -98,11 +73,22 @@ func (suite *IntegrationTestSuite) setupEnv() {
 	suite.Require().NoError(err)
 	suite.srvPort = listener.Addr().(*net.TCPAddr).Port
 	suite.Require().NoError(listener.Close())
-	os.Setenv("PORT", strconv.Itoa(suite.srvPort))
-	os.Setenv("HC_SAMPLE_RATE", "100ms")
-	os.Setenv("HC_GRACE_PERIOD", "5s")
+	suite.Require().NoError(os.Setenv("PORT", strconv.Itoa(suite.srvPort)))
+	log.WithField("port", suite.srvPort).Info("test env setup")
 }
 
-func TestIntegrationSuite(t *testing.T) {
-	suite.Run(t, new(IntegrationTestSuite))
+func (suite *IntegrationTestSuite) isServerHealthy() bool {
+	var resp server.IsHealthyResponse
+
+	result, err := suite.c.R().SetSuccessResult(&resp).Get(fmt.Sprintf("http://localhost:%d/api/is_healthy", suite.srvPort))
+	if err != nil {
+		log.WithError(err).Info("server not ready")
+		return false
+	}
+	if result.StatusCode != http.StatusOK {
+		log.WithField("status code", result.StatusCode).Info("invalid response code")
+		return false
+	}
+
+	return resp.IsHealthy
 }
